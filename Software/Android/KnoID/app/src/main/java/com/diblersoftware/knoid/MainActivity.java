b4326725eba18d2d42ca27ad2bf9bb732722313e
Diego Rodríguez <47021801+diegorgzm@users.noreply.github.com>
2020-09-30 20:52:03 -0600

    Evidencia/Metodologia

---
+++ Software/Android/KnoID/app/src/main/java/com/diblersoftware/knoid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.diblersoftware.knoid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
